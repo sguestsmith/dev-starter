a367e974e1016538a2ad389a4efa3ea48c724dcc
constantino <44826802+msconstantino@users.noreply.github.com>
2024-04-26 11:32:54 -0700

    remove og

--- app/layout.js
+++ app/layout.js
@@ -14,9 +14,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <meta property="og:image" content="/og.png"/>
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
